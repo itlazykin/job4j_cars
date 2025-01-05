ef98e77c32fb265fad644d91c9fc3e620af3e787
itlazykin <dzaka89@gmail.com>
2025-01-05 17:15:36 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,25 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "price_history")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private long before;
+    private long after;
+    private LocalDateTime created;
+}
