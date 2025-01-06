f310bb9b8f3aff648db3f0c0d48e0cefa20af868
itlazykin <dzaka89@gmail.com>
2025-01-06 10:30:25 +0300

    2. ManyToMany [#331986]

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -25,4 +25,11 @@ public class Post {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "auto_post_id")
     private List<Post> priceHistory  = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "post_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
 }
